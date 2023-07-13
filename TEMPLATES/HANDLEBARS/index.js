ba8da96553002d7ee1b6f8c9fb0ad68cfd76b62f
Rayanne Pacto <rayanne.ribeiro@pactonet.com.br>
2023-07-13 12:33:23 -0300

    posr handlebars

--- TEMPLATES/HANDLEBARS/index.js
+++ TEMPLATES/HANDLEBARS/index.js
@@ -1,23 +1,64 @@
-const express = require('express')
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-const exphbs = require('express-handlebars')
+const app = express();
 
-const app = express()
+const hbs = exphbs.create({
+  partialsDir: ["views/partials/"],
+});
 
-app.engine('handlebars', exphbs.engine())
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-app.set('view engine','handlebars')
+app.get("/", function (req, res) {
+  const user = {
+    name: "Matheus",
+    surname: "Battisti",
+  };
 
+  res.render("home", { user: user, auth: true });
+});
 
-app.get('/', (req,res) => {
+app.get("/dashboard", function (req, res) {
+  const items = ["Item a", "Item b", "Item c"];
 
-    const user = {
-        name: "Rayanne",
-        surname: "Ribeiro"
-    }
-    res.render('home', {user: user})
-})
+  res.render("dashboard", { items: items });
+});
 
-app.listen(3000, ()=>{
-    console.log('Rodando na 3000')
-})
+app.get("/post", function (req, res) {
+  const post = {
+    title: "Aprender Node.js",
+    category: "Node.js",
+    body: "Node.js é muito utilizado na programação hoje em dia",
+    comments: 4,
+  };
+
+  res.render("blogpost", { post });
+});
+
+app.get("/blog", function (req, res) {
+  const posts = [
+    {
+      title: "Aprender Node.js",
+      category: "Node.js",
+      body: "Node.js é muito utilizado na programação hoje em dia",
+      comments: 4,
+    },
+    {
+      title: "PHP ainda vale a pena?",
+      category: "PHP",
+      body: "",
+      comments: 12,
+    },
+    {
+      title: "Os segredos de JavaScript",
+      category: "JavaScript",
+      body: "",
+      comments: 5,
+    },
+  ];
+
+  res.render("blog", { posts });
+});
+
+app.listen(3000);
